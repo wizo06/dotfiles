bc561109ac6d83fcb0455679f8a7ff4adc228b2e
wizo06 <14183421+wizo06@users.noreply.github.com>
2022-08-11 09:36:17 -0400

    1.0.2

---
+++ .screenlayout/xrandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --mode 2560x1080 --pos 4480x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1080 --pos 1920x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
